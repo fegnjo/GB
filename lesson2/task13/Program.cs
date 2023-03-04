6c598ccca1739a948119596eff1c73089a16a5f3
1 <aleksey-yagofarov@yandex.ru>
2023-03-04 15:37:39 +0300

    lesson2

---
+++ lesson2/task13/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Enter digit: ");
+int firstDigit = Math.Abs(int.Parse(Console.ReadLine()));
+
+int digit = firstDigit;
+
+while (digit / 100 > 9) {
+    digit /= 10;
+}
+
+digit %= 100;
+digit %= 10;
+
+Console.WriteLine($"{firstDigit} -> {digit}");
+
