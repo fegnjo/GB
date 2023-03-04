fd4229619a09dca640e1cd383997c485261a9352
1 <aleksey-yagofarov@yandex.ru>
2023-03-04 15:52:55 +0300

    lesson2

---
+++ lesson2/taks10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter digit: ");
+int digit = int.Parse(Console.ReadLine());
+
+int hungrets = digit % 100;
+int tens = hungrets / 10;
+
+Console.WriteLine(tens);
--- lesson2/task13/Program.cs
+++ lesson2/task13/Program.cs
@@ -2,14 +2,17 @@
 Console.WriteLine("Enter digit: ");
 int firstDigit = Math.Abs(int.Parse(Console.ReadLine()));
 
-int digit = firstDigit;
+if (firstDigit / 10 > 9) {
+    int digit = firstDigit;
 
-while (digit / 100 > 9) {
-    digit /= 10;
+    while (digit / 100 > 9) {
+        digit /= 10;
+    }
+
+    digit %= 100;
+    digit %= 10;
+
+    Console.WriteLine($"{firstDigit} -> {digit}");
+} else {
+    Console.WriteLine($"{firstDigit} -> третьей цифры нет");
 }
-
-digit %= 100;
-digit %= 10;
-
-Console.WriteLine($"{firstDigit} -> {digit}");
-
